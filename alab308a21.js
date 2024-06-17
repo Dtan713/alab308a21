d5e73891c70501f3327d49f77817fccf7c696a51
Dtan713 <s.donaldtan@gmail.com>
2024-06-16 20:48:41 -0400

    some of the lab

---
+++ alab308a21.js
@@ -0,0 +1,21 @@
+const nestedData = {
+    array: [ 1, 2,]
+
+};
+
+const combinedData = {
+    array: [1 ,2, 3],
+    object: { key: "value" },
+    console.log(combinedData)
+};
+
+class Donald {
+    constructor(Donald, 40) {
+        this.name = name;
+        this.age = age;
+    }
+    console.log(hello, my name is {this.name} {this.age})
+
+}
+
+
